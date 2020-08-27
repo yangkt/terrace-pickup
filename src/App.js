d0ba706bd6993f427c44196ddd465134f3342bf6
ky289 <ky289@cornell.edu>
2020-08-27 14:15:50 -0400

    add build burrito page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './css/App.css';
+import BuildBurrito from './BuildBurrito';
 
 function App() {
   return (
+
+    <>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -20,6 +23,11 @@ function App() {
         </a>
       </header>
     </div>
+
+    <BuildBurrito>
+
+    </BuildBurrito>
+    </>
     
   );
 }
