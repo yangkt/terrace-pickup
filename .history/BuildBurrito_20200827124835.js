57b528badaec1807ecde3fa42f77067bf12ad2c7
Quinnel <helenliu555@gmail.com>
2020-08-27 20:18:34 -0400

    PLS

--- .history/BuildBurrito_20200827124835.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import logo from './logo.svg';
-import './css/App.css';
-
-function BuildBurrito() {
-    
-}
--- .history/BuildBurrito_20200827124845.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import logo from './logo.svg';
-import './css/App.css';
-
-function BuildBurrito() {
-
-}
-
-export BuildBurrito
