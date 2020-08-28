2c58c845275a910468d43013f93158513005d4a9
ky289 <ky289@cornell.edu>
2020-08-28 15:26:37 -0400

    restaurant page

--- react-app/src/RestaurantSide.js
+++ react-app/src/RestaurantSide.js
@@ -1,28 +1,42 @@
 import React, {Component} from 'react';
 import './css/RestaurantSide.css';
 import OrdersLeft from './assets/orders left.png';
+import DoneButton from './assets/done button.png'
 
 class RestaurantSide extends Component {
 
     constructor(props){
         super(props);
         this.state = {
-            order: ''
+            order: 'Burrito Bowl with: \nwhite rice\nchicken\nonions\npeppers'
         }
     }
 
     render() {
         return (
         <div className = 'rest-order-page'>
-            <div className='header-text'>
-                ORDERS TO COMPLETE
-            </div>
             <img src={OrdersLeft} className = 'ing-image' alt='orders left'/>
+            <div className='header-text'>
+                ORDERS TO COMPLETE </div>
+            
             <div className= 'order-main'>
-                
+                <div className = 'text-box'>
+                    <div className = 'text-name'>
+                        JOHN D.
+                    </div>
+                    <div className = 'text-order-number'>
+                        Order # 12345
+                    </div>
+
+                    <div className = 'text-ings'>
+                        {this.state.order}
+
+                    </div>
+                    
+                </div> 
+                <img src={DoneButton} className = 'done-button' alt='done button'/>
             </div>
             
-              
 
         </div>
         );
